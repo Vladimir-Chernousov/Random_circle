ebb56fd3e6fec358deb4a851f6bf3e2b7d41a407
Vladimir-Chernousov <chernousov2005@mail.ru>
2021-11-22 20:45:37 +0300

    Random circle2

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from random import randint
 
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5 import uic
+import UI
 
 
 class MyWidget(QWidget):
     def __init__(self):
         super(MyWidget, self).__init__()
-        uic.loadUi('UI.ui', self)
+        self.ui = UI.Ui_Form()
+        self.ui.setupUi(self)
         qp = QPainter()
         qp.begin(self)
-        self.btn.clicked.connect(self.draw)
+        self.ui.btn.clicked.connect(self.draw)
         self.flag = False
 
     def draw(self):
@@ -25,7 +26,7 @@ class MyWidget(QWidget):
             self.flag = False
             qp = QPainter()
             qp.begin(self)
-            qp.setPen(QColor(255, 255, 0))
+            qp.setPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             qp.drawEllipse(randint(5, 200), randint(5, 200), randint(5, 200), randint(5, 200))
             qp.end()
 
